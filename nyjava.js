d8d0ecca49515f6a506e84f3d4d9c5c62d1cde3e
Petter-Huun <puhuun@gmail.com>
2026-03-06 11:15:22 +0100

    Sier ifra når man vinner

--- nyjava.js
+++ nyjava.js
@@ -19,17 +19,31 @@ kort.forEach(elm => {
 })
 
 function sjekkKort() {
-    let bilde1 = kort1.querySelector(".bak").classList[1]
+    let bilde1 = kort1.querySelector(".bak").classList[1] //finner andre klassen i classlisten til .bak
     let bilde2 = kort2.querySelector(".bak").classList[1]
 
     if (bilde1 !== bilde2) {
         kort1.classList.remove("flip")
         kort2.classList.remove("flip")
+    } else {
+        kort1.classList.add("funnet")
+        kort2.classList.add("funnet")
+
+        sjekkVinn()
     }
 
     kort1 = null
     kort2 = null
 }
 
+function sjekkVinn() {
+    const alleKort = document.querySelectorAll(".kort")
+    const funnet = document.querySelectorAll(".funnet")
+
+    if (alleKort.length == funnet.length) {
+        const seier = document.getElementById("overskrift")
+        seier.innerHTML ="Du vant"
+    }
+}
 
 
